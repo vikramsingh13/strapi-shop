15cb7c4a8cd8930442c6d6510e22064fac791e68
vikramsingh13 <vikramsingh.webdev@gmail.com>
2023-02-27 23:27:29 -0500

    basic architecture

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,37 @@
 import { useEffect } from 'react';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    useLocation
+} from 'react-router-dom';
+import Checkout from './scenes/checkout/Checkout';
+import Home from "./scenes/home/Home";
+import ItemDetails from './scenes/itemDetails/ItemDetails';
+import Confirmation from './scenes/checkout/Confirmation';
+
+const ScrollToTop = () => {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0,0);
+    }, [pathname])
+
+    return null;
+}
 
 const App = () => {
   return (
     <div className="app">
+        <BrowserRouter>
+            <ScrollToTop />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="item/:itemId" element={<ItemDetails />} />
+                <Route path="checkout" element={<Checkout />} />
+                <Route path="checkout/success" element={<Confirmation />} />
+            </Routes>
+        </BrowserRouter>
         This is app container
     </div>
   );
